e98cef123795baf3c7169035d487790e3c84d36f
mgabut <mgabut.pro@gmail.com>
2025-02-25 11:37:07 +0100

    Jest + authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationService } from './core/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Villanelle-website';
+  authenticationService = inject(AuthenticationService)
+
+  email = signal('john.doe@gmail.com')
+  password = signal('qwertz')
+
+  register() {
+    console.log('lancé')
+    this.authenticationService.register('john.doe@gmail.com', 'qwertz')
+      .subscribe((response) => {
+        console.log('Register response:', response)
+      })
+    }
 }
---
+++ src/app/core/authentication.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface RegisterResponse {
+  jwtToken: string;
+  jwtRefreshToken: string;
+  expiresIn: string;
+  userId: string;
+}
+export interface LoginResponse{
+  jwtToken: string;
+  jwtRefreshToken: string;
+  expiresIn: string;
+  userId: string;
+  isRegistered: boolean
+}
+@Injectable()
+
+export abstract class AuthenticationService {
+  abstract register(
+    email: string, 
+    password: string): 
+    Observable<RegisterResponse>;
+
+  abstract login(
+    email: string, 
+    password: string): 
+    Observable<LoginResponse>
+}
