c31d42017ad846eb76f00a20e2e1eafdf5f559a8
mgabut <mgabut.pro@gmail.com>
2025-03-04 11:53:24 +0100

    pages + banniere

---
+++ src/app/visitor/about/about.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about.page.component.html',
+  styleUrl: './about.page.component.scss'
+})
+export class AboutPageComponent {
+
+}
---
+++ src/app/visitor/agenda/agenda.page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-agenda-page',
+  templateUrl: './agenda.page.component.html',
+  styleUrl: './agenda.page.component.scss'
+})
+export class AgendaPageComponent {
+
+}
