554730ec8620b79661c01a34fcb325d83593de6b
mgabut <mgabut.pro@gmail.com>
2025-03-06 11:53:03 +0100

    fix ci + env public

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCbMF84qWOvSZbeqzK6tITsFWg3hF0rLHs",
+        authDomain: "villanelle-website-dev.firebaseapp.com",
+        projectId: "villanelle-website-dev",
+        storageBucket: "villanelle-website-dev.firebasestorage.app",
+        messagingSenderId: "664444670396",
+        appId: "1:664444670396:web:f5adb57ecc35aa16f5dbb9"
+    }
+}
